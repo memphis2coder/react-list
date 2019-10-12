20ab686720dd56fbd0ff4051a5d35a07e36bfa69
Antonio <Memphis2Coder@gmail.com>
2019-10-11 22:37:13 -0500

    Updates

---
+++ precache-manifest.ddd6008d2f60673666bceae49c51e4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4116407fc3cbfa1c9f5a87eef140c1af",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "c3866c1d1c431686b75c",
+    "url": "/react-list/static/css/main.88e26057.chunk.css"
+  },
+  {
+    "revision": "4781fbfda73f7509110a",
+    "url": "/react-list/static/js/2.faeb89c3.chunk.js"
+  },
+  {
+    "revision": "c3866c1d1c431686b75c",
+    "url": "/react-list/static/js/main.79ec7e8c.chunk.js"
+  },
+  {
+    "revision": "7d7486485adebc40d0dd",
+    "url": "/react-list/static/js/runtime-main.e7865a0c.js"
+  }
+]);
